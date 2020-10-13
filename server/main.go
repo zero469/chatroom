bfcc517d8782affec0ff7ca19b95ce99788b45a4
zero469 <1241043065@qq.com>
2020-10-13 16:41:25 +0800

    将读包操作封装为函数

--- server/main.go
+++ server/main.go
@@ -5,49 +5,69 @@ import (
 	"encoding/json"
 	"fmt"
 	"go_code/chapter18/project3/common/message"
+	"io"
 	"net"
 )
 
-//读取客户端发送的信息
-func process(conn net.Conn) {
-	defer conn.Close()
+//读客户端的包，并将包反序列化成结构体
+func readPkg(conn net.Conn) (mes message.Message, err error) {
 	buf := make([]byte, 8096)
 
+	//1 读取mesData的长度
+	_, err = conn.Read(buf[:4])
+	if err != nil {
+		//err = errors.New("read pkg header error")
+		return
+	}
+	pkgLen := int(binary.BigEndian.Uint32(buf[:4]))
+	fmt.Println("服务器读取到mesData的长度 : ", pkgLen)
+
+	//2 读取mesData
+	//TODO: conn.Read()能保证读到这么多的消息吗？如果不指定要读的长度会发生什么？
+	//这里表示期望读到pkgLen这么长的数据，但是实际可能读不到这么多（丢包？？）
+	n, err := conn.Read(buf[:pkgLen])
+	if n != pkgLen || err != nil {
+		//err = errors.New("read pkg body error")
+		return
+	}
+
+	//3. 反序列化message结构体
+	err = json.Unmarshal(buf[:pkgLen], &mes)
+	if err != nil {
+		fmt.Println("json.Unmarshal err=", err)
+		return
+	}
+	return
+}
+
+//处理客户端连接
+func process(conn net.Conn) {
+	defer conn.Close()
+
 	for {
-		//0.1 读取mesData的长度
-		_, err := conn.Read(buf[:4])
+		//1. 读取客户端发送的包并反序列化为结构体
+		mes, err := readPkg(conn)
 		if err != nil {
-			fmt.Println("conn.Read err = ", err)
-			return
-		}
-		pkgLen := int(binary.BigEndian.Uint32(buf[:4]))
-		fmt.Println("服务器读取到mesData的长度 : ", pkgLen)
-
-		//0.2 读取mesData
-		n, err := conn.Read(buf)
-		if n != pkgLen || err != nil {
-			fmt.Println("conn.Read err=", err)
-			return
+			if err == io.EOF {
+				fmt.Println("客户端退出")
+				return
+			} else {
+				fmt.Println("readPkg err=", err)
+				return
+			}
 		}
 
-		//1. 反序列化message结构体
-		var mes message.Message
-		err = json.Unmarshal(buf[:n], &mes)
-		if err != nil {
-			fmt.Println("json.Unmarshal err=", err)
-			return
-		}
+		fmt.Println(mes)
+		// //2. 根据Type反序列化data 结构体
+		// switch mes.Type {
+		// case message.LoginMesType:
+		// 	var loginMes message.LoginMes
+		// 	err = json.Unmarshal([]byte(mes.Data), &loginMes)
+		// 	fmt.Printf("用户id : %v, 用户密码 : %v\n", loginMes.UserId, loginMes.UserPwd)
 
-		//2. 根据Type反序列化data 结构体
-		switch mes.Type {
-		case message.LoginMesType:
-			var loginMes message.LoginMes
-			err = json.Unmarshal([]byte(mes.Data), &loginMes)
-			fmt.Printf("用户id : %v, 用户密码 : %v\n", loginMes.UserId, loginMes.UserPwd)
-
-		default:
-			fmt.Println("未定义Type = ", mes.Type)
-		}
+		// default:
+		// 	fmt.Println("未定义Type = ", mes.Type)
+		// }
 	}
 }
 
